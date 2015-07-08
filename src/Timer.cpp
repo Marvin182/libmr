bb3a31ba8163ff85a419eac2c6627e0d4ab17776
Marvin Ritter <marvin.ritter@gmail.com>
2015-07-08 11:33:28 +0200

    Add util::Timer for measuring times

---
+++ src/Timer.cpp
@@ -0,0 +1,102 @@
+#include "Timer.h"
+#include "../util/assert.h"
+
+namespace util {
+
+Timer::Timer(TimerState state) :
+	state(state),
+	stopwatchTime(0),
+	started(clock_t::duration(0)),
+	stopped(clock_t::duration(0))
+{
+	switch (state) {
+		case TimerState::not_started:
+			// nothing to do here
+			break;
+		case TimerState::started:
+			started = now();
+			break;
+		case TimerState::started_but_paused:
+			// nothing to do here
+			break;
+		case TimerState::stopped:
+			started = now();
+			stopped = started;
+			break;
+		default:
+			assert_unreachable();
+	}
+}
+
+void Timer::start() {
+	assert_error(state == TimerState::not_started, "Timer was already started");
+	restart();
+}
+
+void Timer::restart() {
+	state = TimerState::started;
+	stopwatchTime = clock_t::duration(0);
+	started = now();
+}
+
+void Timer::stop() {
+	assert_error(state == TimerState::started || state == TimerState::started_but_paused, "Timer wasn't running");
+	stopped = now();
+	if (state == TimerState::started_but_paused) {
+		started = stopped;
+	}
+	state = TimerState::stopped;
+}
+
+void Timer::pause() {
+	assert_error(state != TimerState::started_but_paused, "Timer is already paused.");
+	assert_error(state == TimerState::started, "Can't pause stopped timer.");
+	stopwatchTime = elapsed();
+	state = TimerState::started_but_paused;
+}
+
+void Timer::resume() {
+	assert_error(state != TimerState::stopped, "Timer was already stopped.");
+	assert_error(state == TimerState::started_but_paused, "Timer wasn't paused.");
+	state = TimerState::started;
+	started = now();
+}
+
+Timer::clock_t::duration Timer::elapsed() const {
+	switch (state) {
+		case TimerState::not_started:
+			assert_error(false, "Timer was never started.");
+			return clock_t::duration(0);
+		case TimerState::started:
+			return std::chrono::duration_cast<clock_t::duration>(now() - started) + stopwatchTime;
+		case TimerState::started_but_paused:
+			return stopwatchTime;
+		case TimerState::stopped:
+			return std::chrono::duration_cast<clock_t::duration>(stopped - started) + stopwatchTime;
+	}
+
+	assert_unreachable();
+	return clock_t::duration(0);
+}
+
+uint64_t Timer::elapsedSeconds() const {
+	return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::ratio<1, 1>>>(elapsed()).count();
+}
+
+uint64_t Timer::elapsedMilliseconds() const {
+	return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::milli>>(elapsed()).count();
+}
+
+uint64_t Timer::elapsedMicroseconds() const {
+	return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::milli>>(elapsed()).count();
+}
+
+uint64_t Timer::elapsedNanoseconds() const {
+	return std::chrono::duration_cast<std::chrono::duration<uint64_t, std::nano>>(elapsed()).count();
+}
+
+Timer::clock_t::time_point Timer::now() const {
+	return clock_t::now();
+}
+
+} // namespace util
---
+++ src/Timer.h
@@ -0,0 +1,70 @@
+#ifndef MR_TIMER_H
+#define MR_TIMER_H
+
+#include <chrono>
+
+namespace util {
+
+enum class TimerState {
+	not_started,
+	started,
+	started_but_paused,
+	stopped
+};
+
+class Timer {
+public:
+	using clock_t = std::chrono::high_resolution_clock;
+
+	template <typename F>
+	static uint64_t timeMs(F func);
+
+	template <typename F>
+	static uint64_t timeNs(F func);
+
+	Timer(TimerState state = TimerState::not_started);
+
+	void start();
+	void restart();
+	void stop();
+
+	void pause();
+	void resume();
+
+	clock_t::duration elapsed() const;
+
+	uint64_t elapsedSeconds() const;
+	uint64_t elapsedMilliseconds() const;
+	uint64_t elapsedMicroseconds() const;
+	uint64_t elapsedNanoseconds() const;
+
+private:
+	TimerState state;
+	clock_t::duration stopwatchTime;
+	clock_t::time_point started;
+	clock_t::time_point stopped;
+
+	clock_t::time_point now() const;
+};
+
+template <typename F>
+uint64_t Timer::timeMs(F func) {
+	Timer t;
+	t.start();
+	func();
+	t.stop();
+	return t.elapsedMilliseconds();
+}
+
+template <typename F>
+uint64_t Timer::timeNs(F func) {
+	Timer t;
+	t.start();
+	func();
+	t.stop();
+	return t.elapsedNanoseconds();
+}
+
+} // util
+
+#endif // MR_TIMER_H
